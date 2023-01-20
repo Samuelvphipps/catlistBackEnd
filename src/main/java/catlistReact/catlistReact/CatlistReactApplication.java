6c7f1ff50564ed26983e7e0c29cb7e74024b0aa1
Samuelvphipps <samuel.v.phipps@gmail.com>
2023-01-20 10:02:16 -0600

    init

---
+++ src/main/java/catlistReact/catlistReact/CatlistReactApplication.java
@@ -0,0 +1,13 @@
+package catlistReact.catlistReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatlistReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatlistReactApplication.class, args);
+	}
+
+}
---
+++ src/test/java/catlistReact/catlistReact/CatlistReactApplicationTests.java
@@ -0,0 +1,13 @@
+package catlistReact.catlistReact;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatlistReactApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
